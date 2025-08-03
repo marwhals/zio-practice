61c99a93b18beefeec298e7ec3ebd7a89ac2eb2b
marwhals <marjan.mubarok2012@gmail.com>
2025-08-03 21:23:37 +0100

    Testing Basics

---
+++ src/main/scala/part_5_testing/TestingBasics.scala
@@ -0,0 +1,71 @@
+package part_5_testing
+
+import zio._
+import zio.test._
+
+case class Person(name: String, age: Int) {
+  def spellName: String = name.toUpperCase
+  def saySomething: UIO[String] = ZIO.succeed(s"Hi, I'm $name")
+}
+
+object MyTestSpec extends ZIOSpecDefault {
+
+  // fundamental method
+  def spec = test("First Test") {
+    val person = Person("Person", 99)
+
+    // an assertion
+    assert(person.spellName)(Assertion.equalTo("Personz"))
+    // same
+    assertTrue(person.spellName == "Person")
+
+  }
+}
+
+object MyFirstEffectTestSpec extends ZIOSpecDefault {
+  def spec = test("First Effect Test") {
+    val person = Person("Person", 101)
+    assertZIO(person.saySomething)(Assertion.equalTo("Hi, I am a person"))
+    assertZIO(person.saySomething)(Assertion.assertion("should be a greeting")(gr => gr == "Hi, I'm a person"))
+    // doesn't work with assertTrue
+    /*
+    Assertion examples:
+    - Assertion.equalTo => tests for equality
+    - Assertion.assertion => tests any truth value --> the most general assertion
+    - assertion.fails/failsCause -> expects the effect to fail with the exact failure/cause you specify
+    - Assertion.isInterrupted -> validates an interruption
+    - Specialised
+      - isLeft/isRight for Either
+      - isSome/isNone for Option
+      - isSuccess/isFailure for Try
+      - isEmpty/nonEmpty, contains, has* for iterables
+      - isEmptyString/ nonEmptyString, startWithString, matchesRegex for Strings
+     */
+  }
+}
+
+object ASuiteSpec extends ZIOSpecDefault {
+  def spec = suite("Full suite of tests")(
+    // pass multiple tests as arguments
+    test("simple tests") {
+      assertTrue(1 + 3 == 4)
+    },
+    test("a second test") {
+      assertZIO(ZIO.succeed("Scala"))(Assertion.hasSizeString(Assertion.equalTo(5)) && Assertion.startsWithString("S"))
+    },
+    // sub-suites
+    suite("a nested suite")(
+      test("a nested test") {
+        assert(List(1,2,3))(Assertion.isNonEmpty && Assertion.hasSameElements(List(3,1,2)))
+      },
+      test("another nested test") {
+        assert(List(1,2,3).headOption)(Assertion.equalTo(Some(1)))
+      },
+      test("a failed nested test") {
+        assertTrue(1 + 1 == 100)
+      }
+    )
+
+
+  )
+}
