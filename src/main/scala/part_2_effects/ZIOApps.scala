7a7e18e6fc33b294383b25967416b19156e48086
marwhals <marjan.mubarok2012@gmail.com>
2025-08-03 18:02:50 +0100

    ZIO Applications

---
+++ src/main/scala/part_2_effects/ZIOApps.scala
@@ -0,0 +1,42 @@
+package part_2_effects
+
+import zio.{Trace, UIO, Unsafe, ZIO, *}
+
+object ZIOApps {
+
+  val meaningOfLife: UIO[Int] = ZIO.succeed(42)
+
+  // clunky and unsafe
+  def main(args: Array[String]): Unit = {
+    val runtime = Runtime.default
+
+    given trace: Trace = Trace.empty
+
+    Unsafe.unsafeCompat { unsafe =>
+      given u: Unsafe = unsafe
+
+      println(runtime.unsafe.run(meaningOfLife))
+
+    }
+  }
+
+  object BetterApp extends ZIOAppDefault {
+    // provides runtime, trace, ......
+
+    override def run = ZIOApps.meaningOfLife
+
+  }
+
+  // Not needed
+  object ManualApp extends ZIOApp {
+    override implicit def environmentTag = ???
+
+    override type Environment = this.type
+
+    override def bootstrap = ???
+
+    override def run = ???
+
+  }
+
+}
