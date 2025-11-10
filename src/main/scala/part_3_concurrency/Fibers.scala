7fac084fb607ac59ffd89e8ec0507ee0b525a2e4
marwhals <marjan.mubarok2012@gmail.com>
2025-11-10 17:36:29 +0000

    Add fibers exercises

--- src/main/scala/part_3_concurrency/Fibers.scala
+++ src/main/scala/part_3_concurrency/Fibers.scala
@@ -1,7 +1,9 @@
 package part_3_concurrency
 
-import zio.{Exit, Fiber, ZIO, ZIOAppDefault}
 import utils.*
+import zio.{Exit, Fiber, UIO, ZIO, ZIOAppDefault}
+
+import java.io.{File, FileWriter}
 
 /**
  * Paralellism vs Concurrency refresherrrrrrrrrrrr
@@ -28,29 +30,29 @@ object Fibers extends ZIOAppDefault {
     mol <- meaningOfLife.debugThread
     lang <- favLang.debugThread
   } yield (mol, lang)
-  
+
   val differentThreadIO = for {
     _ <- meaningOfLife.debugThread.fork
     _ <- favLang.debugThread.fork
   } yield ()
-  
+
   val meaningOfLifeFiber: ZIO[Any, Nothing, Fiber[Throwable, Int]] = meaningOfLife.fork
-  
+
   // join a fiber
-  def runOnAnotherThread[R,E,A](zio: ZIO[R,E,A]) = for {
+  def runOnAnotherThread[R, E, A](zio: ZIO[R, E, A]) = for {
     fib <- zio.fork
     result <- fib.join
   } yield result
-  
+
   // awaiting a fiber
-  def runOnAnotherThread_v2[R,E,A](zio: ZIO[R,E,A]) = for {
+  def runOnAnotherThread_v2[R, E, A](zio: ZIO[R, E, A]) = for {
     fib <- zio.fork
     result <- fib.await
   } yield result match {
     case Exit.Success(value) => s"succeeded with $value"
     case Exit.Failure(cause) => s"failed with $cause"
   }
-  
+
   // poll - peek at the result of the fiber right now without blocking
   val peekFiber = for {
     fib <- ZIO.attempt {
@@ -59,7 +61,7 @@ object Fibers extends ZIOAppDefault {
     }.fork
     result <- fib.poll
   } yield result
-  
+
   // compose fibers
   // zip
   val zippedFibers = for {
@@ -68,7 +70,7 @@ object Fibers extends ZIOAppDefault {
     fiber = fib1.zip(fib2)
     tuple <- fiber.join
   } yield tuple
-  
+
   // or else
   val chainedFibers = for {
     fiber1 <- ZIO.fail("not good!").debugThread.fork
@@ -77,6 +79,96 @@ object Fibers extends ZIOAppDefault {
     message <- fiber.join
   } yield message
 
-  def run = chainedFibers.debugThread
-//  def run = runOnAnotherThread(meaningOfLife).debugThread
+  /**
+   * Exercises
+   */
+  // 1 - zip two fibers without using the zip combinator
+  // hint: create a fiber that waits for both
+  def zipFibers[E, A, B](fiber1: Fiber[E, A], fiber2: Fiber[E, B]): ZIO[Any, Nothing, Fiber[E, (A, B)]] = {
+    val finalEffect = for {
+      v1 <- fiber1.join
+      v2 <- fiber2.join
+    } yield (v1, v2)
+    finalEffect.fork
+  }
+
+  val zippedFibers_v2 = {
+    for {
+      fib1 <- ZIO.succeed("Result from fiber 1").debugThread.fork
+      fib2 <- ZIO.succeed("Result from fiber 2").debugThread.fork
+      fiber <- zipFibers(fib1, fib2)
+      tuple <- fiber.join
+    } yield tuple
+  }
+
+  // Generalise for different error types
+  def zipFibersGeneral[E, E1 <: E, E2 <: E, A, B](fiber1: Fiber[E1, A], fiber2: Fiber[E2, B]): ZIO[Any, Nothing, Fiber[E, (A, B)]] = {
+    val finalEffect = for {
+      v1 <- fiber1.join
+      v2 <- fiber2.join
+    } yield (v1, v2)
+    finalEffect.fork
+  }
+
+  // 2 - same thing with orElse
+  def chainFibers[E, A](fiber1: Fiber[E, A], fiber2: Fiber[E, A]): ZIO[Any, Nothing, Fiber[E, A]] = {
+    val waitFiber1 = fiber1.join
+    val waitFiber2 = fiber2.join
+    val finalEffect = waitFiber1.orElse(waitFiber2)
+    finalEffect.fork
+  }
+
+  def chainFibers_v2[E, A](fiber1: Fiber[E, A], fiber2: Fiber[E, A]): ZIO[Any, Nothing, Fiber[E, A]] =
+    fiber1.join.orElse(fiber2.join).fork
+
+  // 3 - distributing a task in between many fibers
+  // spawn n fibers (= nFiles each fiber reads one file)
+  // count the number of words in each file, then aggregate all the results together in one big number
+  def generateRandomFile(path: String): Unit = {
+    val random = scala.util.Random
+    val chars = 'a' to 'z'
+    val nWords = random.nextInt(2000) // at most 2000 random words
+
+    val content = (1 to nWords)
+      .map(_ => (1 to random.nextInt(10)).map(_ => chars(random.nextInt(26))).mkString) // one word for every 1 to nWords
+      .mkString(" ")
+
+    val writer = new FileWriter(new File(path))
+    writer.write(content)
+    writer.flush()
+    writer.close()
+  }
+
+  // part 1 - an effect which reads one file and counts the words there
+  def countWords(path: String): UIO[Int] = {
+    ZIO.succeed {
+      val source = scala.io.Source.fromFile(path)
+      val nWords = source.getLines().mkString(" ").split(" ").count(_.nonEmpty)
+      println(s"Counted $nWords in $path")
+      source.close()
+      nWords
+    }
+  }
+
+  // part 2 - spin up fibers for all the files
+  def wordCountParallel(n: Int): UIO[Int] = {
+    val effects: Seq[ZIO[Any, Nothing, Int]] = (1 to n)
+      .map(i => s"src/main/scala/resources/testfile_$i.txt")
+      .map(countWords) // list of effects
+      .map(_.fork) // list of effects returning fibers
+      .map((fiberEff: ZIO[Any, Nothing, Fiber[Nothing, Int]]) => fiberEff.flatMap(_.join)) // list of effects returning values (count of words)
+
+    effects.reduce { (zioa, ziob) =>
+      for {
+        a <- zioa
+        b <- ziob
+      } yield a + b
+    }
+  }
+
+  //  def run = chainedFibers.debugThread
+  //  def run = runOnAnotherThread(meaningOfLife).debugThread
+//  def run = ZIO.succeed((1 to 10).foreach(i => generateRandomFile(s"src/main/scala/resources/testfile_${i}.txt")))
+    def run = wordCountParallel(10).debugThread
 }
+
